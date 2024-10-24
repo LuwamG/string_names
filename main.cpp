d3f23febf63172591ce06d1d49cd23ae36287b07
LuwamG <30065081@cc.peralta.edu>
2024-10-23 23:06:38 -0700

    codesAdd .gitattributes, README.md, and LICENSE.txt.

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include <string>
+#include "string_name.hpp"
+using namespace std;
+int main() {
+	string first_name;
+	string last_name;
+	string_name(first_name, last_name);
+
+	return 0;
+}
