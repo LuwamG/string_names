eb98d607a512025ec76043c39b25f1f326fa6230
LuwamG <30065081@cc.peralta.edu>
2024-11-17 01:06:34 -0800

    remove gitattribute

--- string_names.cpp
+++ string_names.cpp
@@ -3,20 +3,21 @@
 using namespace std;
 
 string name;
-
+// function to print first and second names separately
 void string_name(string& first_name, string& last_name) {
 	cout << "Enter your full name: ";
 	getline(cin, name);
-	for (int i = 0; i < name.length(); i++) {
-		if (name[i] == ' ') {
-			first_name = name.substr(0, i);
-			last_name = name.substr(i, name.length() - 1);
-		}
-		else {
-			first_name = name;
-			last_name = ' ';
-		}
+    size_t space_pos = name.find(' ');
+
+    
+    if (space_pos != string::npos) {
+        first_name = name.substr(0, space_pos); 
+        last_name = name.substr(space_pos + 1); 
+    }
+    else {
+        first_name = name; 
+        last_name = "";
+    }
 		cout << "First name: " << first_name << endl;
 		cout << "Last name: " << last_name << endl;
 	}
-}
